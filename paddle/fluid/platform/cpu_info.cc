1b9a0742bad9e3f0922bfcd823bad86c0fe6d472
Zhang Na <zhangna@loongson.cn>
2023-03-02 11:24:23 +0800

    add loongarch support

--- paddle/fluid/platform/cpu_info.cc
+++ paddle/fluid/platform/cpu_info.cc
@@ -165,7 +165,7 @@ bool MayIUse(const cpu_isa_t cpu_isa) {
     return true;
   } else {
 #if !defined(WITH_NV_JETSON) && !defined(PADDLE_WITH_ARM) && \
-    !defined(PADDLE_WITH_SW) && !defined(PADDLE_WITH_MIPS)
+    !defined(PADDLE_WITH_SW) && !defined(PADDLE_WITH_MIPS) && !defined(PADDLE_WITH_LOONGARCH)
     int reg[4];
     cpuid(reg, 0);
     int nIds = reg[0];
